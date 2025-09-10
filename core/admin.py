9f92bb7fce90b4f78096699ce40fa1e27d93bd40
matheus lobo <klebercamara12@gmail.com>
2025-09-10 10:47:43 -0400

    Models e Admin criados (possíveis alterações futuras)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
+# core/admin.py
 from django.contrib import admin
+from .models import Empresa, Cliente
 
-# Register your models here.
+admin.site.register(Empresa)
+admin.site.register(Cliente)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
+# core/models.py
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    nome_empresa = models.CharField(max_length=200, verbose_name="Nome da Empresa")
+    cnpj = models.CharField(max_length=18, blank=True, null=True, verbose_name="CNPJ")
+
+    def __str__(self):
+        return self.nome_empresa
+
+    class Meta:
+        verbose_name = "Empresa"
+        verbose_name_plural = "Empresas"
+        ordering = ['nome_empresa']
+
+
+class Cliente(models.Model):
+    nome_cliente = models.CharField(max_length=200, verbose_name="Nome do Cliente")
+    cpf = models.CharField(max_length=14, blank=True, null=True, verbose_name="CPF")
+    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Empresa")
+
+    def __str__(self):
+        return self.nome_cliente
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+        ordering = ['nome_cliente']
