c4607732df1d453221bdddff2e32095ff1fe0d91
matheus lobo <klebercamara12@gmail.com>
2025-09-10 11:11:25 -0400

    Rotas criadas e testadas

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,13 @@ from django.views.generic import RedirectView
 from .views import (
     EmpresaListView, EmpresaCreateView, EmpresaUpdateView, EmpresaDeleteView,
     ClienteListView, ClienteCreateView, ClienteUpdateView, ClienteDeleteView,
+    EntradaListView, EntradaCreateView, EntradaUpdateView, EntradaDeleteView,
+    SaidaListView, SaidaCreateView, SaidaUpdateView, SaidaDeleteView,
 )
 
 urlpatterns = [
     # Redireciona a raiz do site para a lista de clientes
-    path('', RedirectView.as_view(pattern_name='cliente-list', permanent=False)),
+    path('', RedirectView.as_view(pattern_name='saida-list', permanent=False)),
 
     # Rotas de Empresa
     path('empresas/', EmpresaListView.as_view(), name='empresa-list'),
@@ -20,4 +22,16 @@ urlpatterns = [
     path('clientes/novo/', ClienteCreateView.as_view(), name='cliente-create'),
     path('clientes/<int:pk>/editar/', ClienteUpdateView.as_view(), name='cliente-update'),
     path('clientes/<int:pk>/deletar/', ClienteDeleteView.as_view(), name='cliente-delete'),
+
+    # Rotas de Entrada
+    path('entradas/', EntradaListView.as_view(), name='entrada-list'),
+    path('entradas/nova/', EntradaCreateView.as_view(), name='entrada-create'),
+    path('entradas/<int:pk>/editar/', EntradaUpdateView.as_view(), name='entrada-update'),
+    path('entradas/<int:pk>/deletar/', EntradaDeleteView.as_view(), name='entrada-delete'),
+
+    # Rotas de Saída
+    path('saidas/', SaidaListView.as_view(), name='saida-list'),
+    path('saidas/nova/', SaidaCreateView.as_view(), name='saida-create'),
+    path('saidas/<int:pk>/editar/', SaidaUpdateView.as_view(), name='saida-update'),
+    path('saidas/<int:pk>/deletar/', SaidaDeleteView.as_view(), name='saida-delete'),
 ]
